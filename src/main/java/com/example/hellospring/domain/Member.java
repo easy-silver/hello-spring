cb98c276b33b9697828c859ad3cb376e0261fab1
Timo <developer.timo@gmail.com>
2020-12-20 08:33:00 +0900

    JPA Repository 추가

--- src/main/java/com/example/hellospring/domain/Member.java
+++ src/main/java/com/example/hellospring/domain/Member.java
@@ -1,8 +1,14 @@
 package com.example.hellospring.domain;
 
+import javax.persistence.*;
+
+@Entity
 public class Member {
 
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY )
     private Long id;
+
+    //@Column(name = "uesrname")
     private String name;
 
     public Long getId() {
